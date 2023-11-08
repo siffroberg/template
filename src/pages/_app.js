dbd8902026bf06a4428d54b883f6d73a485fc4ef
siffroberg <117364387+siffroberg@users.noreply.github.com>
2023-11-08 12:51:20 +0100

    Initial commit

---
+++ src/pages/_app.js
@@ -0,0 +1,11 @@
+import "@/styles/globals.css";
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <MantineProvider>
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
+}
